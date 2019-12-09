665823ed3b37d01923b1da80fa27fa2089c28506
wxg <wuxianggan@youxiuqn.com>
2019-12-09 17:08:32 +0800

    string test

--- src/test/java/com/example/demo/java/StringTest.java
+++ src/test/java/com/example/demo/java/StringTest.java
@@ -22,4 +22,17 @@ public class StringTest {
         System.out.println(str1+str2);
         //java会自动创建并调用StringBuilder.append拼接字符串(jdk1.5之前使用StringBuffer)
     }
+
+    @Test
+    public void test3(){
+        String str1 = new String("void");
+        String str2 = "void";
+        String str3 = "void";
+        System.out.println(str1==str2);
+        System.out.println(str2==str3);
+        str1 = str1.intern();
+        //显式加入常量池并且改变str1的指向
+        System.out.println(str1==str2);
+        System.out.println(str1==str3);
+    }
 }
